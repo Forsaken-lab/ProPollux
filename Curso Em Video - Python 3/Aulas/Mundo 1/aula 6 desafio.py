ccede7a095504ee2ddc130cd4b645555828c81b3
Forsaken-lab <57645917+Forsaken-lab@users.noreply.github.com>
2019-11-25 07:31:01 -0300

    aula 6 desafio

--- Curso Em Video - Python 3/Aulas/Mundo 1/aula 6 desafio.py
+++ Curso Em Video - Python 3/Aulas/Mundo 1/aula 6 desafio.py
@@ -1,2 +1,4 @@
 memoria = input('digite algo aqui ')
 print(type(memoria))
+print(memoria.isalnum(), memoria.isnumeric(), memoria.isalpha(), memoria.isupper(), memoria.islower(), memoria.isascii(),
+      memoria.isdecimal())
