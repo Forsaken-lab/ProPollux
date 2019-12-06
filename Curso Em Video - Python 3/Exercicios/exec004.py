e197fe162d27b58b21eefacbc29607e9b4e6e6c0
Forsaken-lab <57645917+Forsaken-lab@users.noreply.github.com>
2019-12-06 15:19:11 -0300

    aula 6 desafio

---
+++ Curso Em Video - Python 3/Exercicios/exec004.py
@@ -0,0 +1,4 @@
+# Exec 004 - curso python - mostrar informaçoes (tipos primitivo)
+a = input('Escreva algo e tenha uma surpressa ')
+print('O que voce escreveru é ')
+print('AlphaNumerico {} \n inteiro {} \n está em minusculo {}'.format(a.isalnum(),a.isnumeric(), a.islower()))
---
+++ Curso Em Video - Python 3/Exercicios/exec03.py
@@ -0,0 +1,8 @@
+#Aula 03 de python 3 - exercicio 003
+print('Somando inteiros')
+n1 = int(input('Escreva o primeiro numero '))
+n2 = int(input('Escreva o segundo numero '))
+
+r = n1 + n2
+
+print('a soma de {} com {} é de {}'.format(n1, n2, r))
