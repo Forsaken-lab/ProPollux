73cd78728b3f805ae91e59b00bcfb63838d18ba9
Forsaken-lab <57645917+Forsaken-lab@users.noreply.github.com>
2019-12-07 19:11:45 -0300

    aula 6 desafio - atualizado

--- Curso Em Video - Python 3/Exercicios/exec004.py
+++ Curso Em Video - Python 3/Exercicios/exec004.py
@@ -1,4 +1,4 @@
-# Exec 004 - curso python - mostrar informaçoes (tipos primitivo)
+# Exec 004 - curso python - mostrar informaçoes (tipos primitivo) V0.2
 a = input('Escreva algo e tenha uma surpressa \n')
 print('O que voce escreveru é ')
 print('Número? : {} \n Texto? : {} \n Maiusculas? : {} \n Minusculas? : {} \n Indetificavel? : {}'.format(a.isnumeric(), a.isalpha(), a.isupper(),a.islower(),a.isidentifier()))
